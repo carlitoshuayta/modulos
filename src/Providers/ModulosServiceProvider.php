3f70ca80dd83b9fb066d56b9a1948a9244e0c813
Carlos huayta <carloshuayta112@gmail.com>
2019-04-09 21:11:00 -0400

    paquete

---
+++ src/Providers/ModulosServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace rols_permissions\modulos\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModulosServiceProvider extends ServiceProvider{
+    public function boot(){
+    dd("que tal amiguitos");
+    }
+    public function register()
+    {
+
+    }
+}
